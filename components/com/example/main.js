ae705d7e401fd6717dcb7c096bab114e8a14cece
Micha Niskin <asdf@ubergibson.com>
2009-03-19 01:59:45 -0400

    added flickr component

--- components/com/example/main.js
+++ components/com/example/main.js
@@ -1,4 +1,20 @@
 
 function(argv) {
-  $(".contentDiv").append("<p>argv was '"+$.golf.toJSON(argv)+"'</p>");
+  var self = this;
+  var flickr = new Component.com.example.flickr();
+
+  this.load = function(tag) {
+    $(".title").text("Look, it's "+tag+"!");
+    flickr.load(tag);
+  };
+
+  $("form").submit(function() {
+      self.load($("[name='what']").val());
+      return false;
+  });
+
+  if (argv.length == 0) argv = ["golf"];
+  $(".pix").append(flickr);
+
+  this.load(argv[0]);
 }
