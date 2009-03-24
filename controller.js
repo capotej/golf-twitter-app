f486964d531a06bb7680b9ad2131c6e0f6fa2f4b
Micha Niskin <asdf@ubergibson.com>
2009-03-24 16:45:42 -0400

    changed over to new 'controller' property from the old 'controllers' property

--- controller.js
+++ controller.js
@@ -1,5 +1,7 @@
 
-jQuery.golf.controllers = {
+jQuery.golf.homeActionName = "search";
+
+jQuery.golf.controller = {
 
   defaultAction: function(argv, b, match) {
     var a = new Component.com.example.main(argv);
