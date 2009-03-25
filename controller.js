dc8317fc943e2f1a95e7683b3a4aca1227d0edda
Micha Niskin <asdf@ubergibson.com>
2009-03-25 16:31:49 -0400

    updates to controller to reflect golf API changes

--- controller.js
+++ controller.js
@@ -1,10 +1,18 @@
 
-jQuery.golf.homeActionName = "search";
+// this is the default action name, in case no action was specified
+jQuery.golf.defaultActionName = "search";
 
+// this defines the golf controller
 jQuery.golf.controller = {
 
-  defaultAction: function(argv, b, match) {
-    var a = new Component.com.example.main(argv);
+  "^hello/(.*)$": function(b, match) {
+    b.append("<h1>Hello, there!</h1>");
+    b.append("<p>The arguments are: 1. '"+match[0]+"', 2. '"+match[1]+"'</p>");
+  },
+
+  // the default action
+  defaultAction: function(b, match) {
+    var a = new Component.com.example.main(match);
     b.append(a);
   }
 
