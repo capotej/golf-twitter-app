6d0c4c7b8921760a31287b1255bd4a6f46d3c050
Julio Capote <capotej@pancake.local>
2009-12-27 00:36:50 -0500

    init

--- controller.js
+++ controller.js
@@ -1,30 +1,10 @@
-
-var data = [
-  [ "22", 
-    "first post", 
-    "<p>This is the first post.</p>"
-  ],
-  [ "23", 
-    "second post", 
-    "<p>This is the second post.</p>"
-  ],
-  [ "24", 
-    "fourth post", 
-    "<p>This is the fourth post.</p>"
-  ],
-  [ "25", 
-    "fifth post", 
-    "<p>This is the fifth post.</p>"
-  ]
-];
-
-$.golf.defaultRoute = "/test/";
+//$.golf.defaultRoute = "/test/";
 
 $.golf.controller = [
 
   { route: ".*",
     action: function(b,match) {
-      b.empty().append(new Component.Example(data));
+      b.empty().append(new Component.Twitter());
     }
   }
 
